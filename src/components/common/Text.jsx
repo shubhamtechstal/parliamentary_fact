4c4e154b78f4948c4942a61d57127e81c06afcb7
Rishab Siyote <rishabh.siyote@techstalwarts.com>
2024-05-30 11:07:48 +0530

    conflict solve

--- src/components/common/Text.jsx
+++
@@ -1,7 +0,0 @@
-import { Typography } from '@mui/material';
-import React from 'react';
-
-export default function Text({ text, sx }) {
-  const style = { ...sx, fontFamily: '"Oxygen", sans-serif' };
-  return <Typography sx={style}>{text}</Typography>;
-}
