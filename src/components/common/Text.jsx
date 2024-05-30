6f55586e3ab8db7838930fc29133b27dac32c043
Rishab Siyote <rishabh.siyote@techstalwarts.com>
2024-05-30 10:55:23 +0530

    readme and package-lock.json file remove

--- src/components/common/Text.jsx
+++ src/components/common/Text.jsx
@@ -1,9 +1,7 @@
-import { Typography } from '@mui/material'
-import React from 'react'
+import { Typography } from '@mui/material';
+import React from 'react';
 
-export default function Text({text,sx}) {
-    const style = {...sx,fontFamily:'"Oxygen", sans-serif'}
-  return (
-    <Typography sx={style}>{text}</Typography>
-  )
+export default function Text({ text, sx }) {
+  const style = { ...sx, fontFamily: '"Oxygen", sans-serif' };
+  return <Typography sx={style}>{text}</Typography>;
 }
